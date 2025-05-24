a5b17bc5ed61d967ec0861511c8602037f8ca3c1
DEVnitishOfficial <nitish.naroun123@gmail.com>
2025-05-25 01:06:07 +0530

    optimize codebase and connect to mongo db

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+export const signup = (req, res) => {
+  // Logic for handling user signup
+  res.send('User signed up successfully');
+}
+export const login = (req, res) => {
+  // Logic for handling user login
+  res.send('User logged in successfully');
+}
+export const logout = (req, res) => {
+  // Logic for handling user logout
+  res.send('User logged out successfully');
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,23 +1,19 @@
 import express from "express";
 import dotenv from 'dotenv'
 
+import authRoutes from "./routes/auth.route.js";
+import { connectDB } from "./lib/db.js";
+
+
 dotenv.config()
 
 const app = express();
 const port = process.env.PORT
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-app.get("/api/auth/signup",(req,res) => {
-    res.send("signup page")
-})
-app.get("/api/auth/login", (req, res) => {
-  res.send("Login page");
-});
-app.get("/api/auth/logout",(req, res) => {
-    res.send("Logout page")
-})
+app.use('/api/auth', authRoutes);
+
+
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
+  connectDB();
 });
